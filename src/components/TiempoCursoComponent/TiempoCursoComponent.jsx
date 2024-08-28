1251ae54a53803fa16cd83cef27fdbd7db33d85b
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2024-08-28 16:35:57 -0300

    asdasdads

--- src/components/TiempoCursoComponent/TiempoCursoComponent.jsx
+++ src/components/TiempoCursoComponent/TiempoCursoComponent.jsx
@@ -1,5 +1,8 @@
 export const TiempoCursoComponent = () => {
-    return(
-        <h1>Estoy en el tiempo 2:43:08</h1>
+    return (
+        <>
+            <h1>Estoy en el tiempo 3:02:51</h1>
+            <h2>Terminada la primera parte, ir a otro proyecto.</h2>
+        </>
     );
 }
